042e0f0d99914e70a5c8794e440455b5f31bb541
hongsup <hongsup@d762def4-4115-401c-af51-09bd88a62f24>
2011-12-30 21:12:16 +0000

    test renaim

--- test_compiler/errors/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILES=*.gm
-for f in $FILES
-do
-   echo "doing $f"
-   cat $f > output/$f.txt
-   ./gm_comp -DS=3 -V=1 $f >> output/$f.txt
-done
--- test_compiler/parse/README.txt
+++
@@ -1,1 +0,0 @@
-Test of Parsing and Type-Resolution
