2d555df174528d36df0ed8d5eb12d844e410f994
Sungpack Hong <hongsup@gmail.com>
2012-01-04 15:21:13 -0800

    making directory scripts

---
+++ make_dirs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+OJB=obj
+CPP=apps/output_cpp
+LIB=apps/output_cpp/gm_graph
+BUILD_DIRS="obj $CPP/bin $CPP/generated $CPP/bin $CPP/data $LIB/lib"
+for D in $BUILD_DIRS
+do
+    if [ ! -d  ${D} ]
+    then
+        mkdir ${D}
+    fi
+
+done
+
+TEST_DIRS="cpp_be errors gps opt parse rw_check sugars"
+for D in $TEST_DIRS
+do
+    if [ ! -d  test/${D}/output ]
+    then
+        mkdir "test/${D}/output"
+    fi
+
+done
+
+
