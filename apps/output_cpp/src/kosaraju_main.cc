c0d2211d0aed04738c8ed4e8b0e912493a9927aa
Sungpack Hong <hongsup@gmail.com>
2012-01-04 16:05:55 -0800

    adding omitted files

---
+++ apps/output_cpp/src/kosaraju_main.cc
@@ -0,0 +1,64 @@
+#include "common_main.h"
+#include "kosaraju.h"  // defined in generated
+
+class my_main: public main_t
+{
+public:
+    int* membership;
+    int  num_membership;
+
+    my_main()
+    {
+    }
+
+    virtual bool prepare() {
+        membership = new int[G.num_nodes()];
+        num_membership = 0;
+    }
+
+    virtual bool run() 
+    {
+        num_membership = kosaraju(G, membership);
+        return true;
+    }
+
+    virtual bool post_process() {
+        printf("num_membership = %d\n", num_membership);
+    }
+};
+
+#define TEST_LARGE  1
+
+int main(int argc, char** argv)
+{
+#if TEST_LARGE
+    my_main M;
+    M.main(argc, argv);
+#else
+
+   gm_graph G;
+   for(int i=0;i<10;i++)
+        G.add_node();
+
+    G.add_edge(0,1);
+    G.add_edge(1,2);
+    G.add_edge(2,3);
+    G.add_edge(3,0);
+    G.add_edge(5,8);
+    G.add_edge(7,4);
+    G.add_edge(9,1);
+    G.add_edge(1,4);
+    G.add_edge(2,5);
+    G.add_edge(4,9);
+
+    int* membership;
+    int  num_membership;
+
+    membership = new int[G.num_nodes()];
+    num_membership = 0;
+
+    num_membership = kosaraju(G, membership);
+    printf("num_membership = %d\n", num_membership);
+#endif
+}
+
--- make_dirs.sh
+++ make_dirs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 OJB=obj
+BIN=bin
 CPP=apps/output_cpp
 LIB=apps/output_cpp/gm_graph
-BUILD_DIRS="obj $CPP/bin $CPP/generated $CPP/bin $CPP/data $LIB/lib"
+BUILD_DIRS="bin obj $CPP/bin $CPP/generated $CPP/bin $CPP/data $LIB/lib"
 for D in $BUILD_DIRS
 do
     if [ ! -d  ${D} ]
