74c2ccdc2f66e53a90078b9ce4556c85ed0c9abf
Martin Sevenich <sevenich@in.tum.de>
2012-07-13 15:23:55 -0700

    added examples for new features / new algorithms

--- apps/Programs.mk
+++ apps/Programs.mk
@@ -5,7 +5,7 @@ ifndef PROGS
   ifeq ($(TARGET), giraph)
     PROGS= pagerank avg_teen_cnt conduct hop_dist sssp random_bipartite_matching bc_random
   else
-    PROGS= pagerank avg_teen_cnt conduct bc kosaraju adamicAdar v_cover sssp #random_bipartite_matching
+    PROGS= sssp_path randomnodesampling trianglecounting pagerank avg_teen_cnt conduct bc kosaraju adamicAdar v_cover sssp #random_bipartite_matching
   endif
  endif
 endif
