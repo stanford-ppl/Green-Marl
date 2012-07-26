865948da75d3a654fb2932c790809a65fbe81ec6
Adam Welc <adam.welc@oracle.com>
2012-07-26 11:26:39 -0700

    A more universally accepted format of typeof.

--- apps/output_cpp/gm_graph/platform/x86/inc/gm_platform_helpers.h
+++ apps/output_cpp/gm_graph/platform/x86/inc/gm_platform_helpers.h
@@ -3,11 +3,11 @@
 
 #define _gm_CAS_asm(ptr, oldval, newval) \
     ({ \
-      typeof(ptr) ___p = (ptr); \
-      typeof(*___p) ___oldval = (oldval); \
-      typeof(*___p) ___newval = (newval); \
+      __typeof__(ptr) ___p = (ptr); \
+      __typeof__(*___p) ___oldval = (oldval); \
+      __typeof__(*___p) ___newval = (newval); \
       register unsigned char ___result; \
-      register typeof(*___p) ___readval; \
+      register __typeof__(*___p) ___readval; \
       if (sizeof(*___p) == 4) { \
         __asm__ __volatile__ ("lock; cmpxchgl %3,%1; sete %0" \
                               : "=q"(___result), "=m"(*___p), "=a"(___readval) \
