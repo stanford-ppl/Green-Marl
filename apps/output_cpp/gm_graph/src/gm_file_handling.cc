ed319034e984df153da1de45eb8cfbd6720c322b
navahgar <raghavan.raman@oracle.com>
2012-11-14 14:09:21 -0800

    Fixed the classpath in gm_file_handling. The extra space at the end of the class path string caused problems in accessing the class files in javabin."

--- apps/output_cpp/gm_graph/src/gm_file_handling.cc
+++ apps/output_cpp/gm_graph/src/gm_file_handling.cc
@@ -40,7 +40,7 @@ GM_JNI_Handler::GM_JNI_Handler() {
     const char* LoggingJar = "commons-logging-1.0.4.jar";
     const char* GuavaJar = "guava-r09-jarjar.jar";
 
-    sprintf(buffer, "-Djava.class.path=%s/%s:%s/lib/%s:%s/lib/%s:%s/apps/output_cpp/gm_graph/javabin/ -XX:+UseOSErrorReporting", 
+    sprintf(buffer, "-Djava.class.path=%s/%s:%s/lib/%s:%s/lib/%s:%s/apps/output_cpp/gm_graph/javabin/", 
             HadoopHome, HadoopCoreJar,
             HadoopHome, LoggingJar,
             HadoopHome, GuavaJar, 
