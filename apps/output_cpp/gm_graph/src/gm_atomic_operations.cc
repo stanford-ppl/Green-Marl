c60cbd3642aac2439847b8290872522d0e5d0139
Martin Sevenich <sevenich@in.tum.de>
2012-12-11 02:21:03 -0800

    fixing bug in ATOMIC_AND and ATOMIC_OR

--- apps/output_cpp/gm_graph/src/gm_atomic_operations.cc
+++ apps/output_cpp/gm_graph/src/gm_atomic_operations.cc
@@ -1,9 +1,9 @@
 #include "gm_atomic_wrapper.h"
 
 void ATOMIC_AND(bool* target, bool value) {
-    if ((!value) || (*target)) *target = false;
+    if (!value && *target) *target = false;
 }
 
 void ATOMIC_OR(bool* target, bool value) {
-    if ((value) && (*target)) *target = true;
+    if (value && !(*target)) *target = true;
 }
