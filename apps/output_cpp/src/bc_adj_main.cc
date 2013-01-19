0e0c3cfb27a0215b1e7a21b38602a7edf2900052
Adam Welc <adam.welc@oracle.com>
2013-01-18 19:39:51 -0800

    The BC version using adjacency list now takes a configuration file as parameter that contains information about nodes whose BC is to be printed.

--- apps/output_cpp/src/bc_adj_main.cc
+++ apps/output_cpp/src/bc_adj_main.cc
@@ -1,17 +1,24 @@
 #include "common_main.h"
+//#include "bc.h"  // defined in generated
 #include "bc_adj.h"  // defined in generated
+#include "gm_rand.h"
+#include "gm_graph.h"
+#include "gm_util.h"
+#include "gm_file_handling.h"
 
 int main(int argc, char** argv) {
 
     if (argc < 4) {
-        printf ("Usage: ./sssp_path <input_file> <node_id> <num_threads>\n");
+        printf ("Usage: ./sssp_path <input_file> <config_file> <num_threads>\n");
         exit(1);
     }
     char *inputFile = argv[1];
+    char *configFile = argv[2];
 
     int num = atoi(argv[3]);
     printf("running with %d threads\n", num);
     gm_rt_set_num_threads(num); // gm_runtime.h
+
 
     //------------------------------
     // Empty graph creation
@@ -52,16 +59,58 @@ int main(int argc, char** argv) {
     //    printf ("Number of nodes = %d\n", G.num_nodes());
     //    printf ("Number of edges = %d\n", G.num_edges());
 
-    node_t root_node_key = atol(argv[2]);
-    node_t root_node_id = G.nodekey_to_nodeid(root_node_key);
+    //------------------------------
+    // Read seeds and result nodes from the config file
+    //------------------------------
+
+    
+    std::string line, temp_str;
+    const char* separators = " \t";
+    // Open the file
+    GM_Reader lineReader(configFile, false);
+    if (lineReader.failed()) {
+        return false;
+    }
+
+    /*
+    gm_node_seq Seeds;
+    lineReader.getNextLine(line);
+    assert(!line.empty());
+    GM_Tokenizer s_tknzr(line, separators);
+    while (s_tknzr.hasNextToken()) {
+      temp_str = s_tknzr.getNextToken();
+      Seeds.push_back(G.nodekey_to_nodeid(atol(temp_str.c_str())));
+    }
+    */
+
+    gm_node_seq Outputs;
+    lineReader.getNextLine(line);
+    assert(!line.empty());
+    GM_Tokenizer o_tknzr(line, separators);
+    while (o_tknzr.hasNextToken()) {
+      temp_str = o_tknzr.getNextToken();
+      Outputs.push_back(G.nodekey_to_nodeid(atol(temp_str.c_str())));
+    }
+
+
 
     float* BC = new float[G.num_nodes()];
-
-
     struct timeval T1, T2;    
     gettimeofday(&T1, NULL);
+    //    comp_BC(G, BC, Seeds);
     comp_BC(G, BC);
     gettimeofday(&T2, NULL);
     printf("BC (threads: %d) - COMPUTATION RUNNING TIME (ms): %lf\n", num, (T2.tv_sec - T1.tv_sec) * 1000 + (T2.tv_usec - T1.tv_usec) * 0.001);
-    printf("BC for node %d : %f\n", root_node_key, BC[root_node_id]); 
+
+    gm_node_seq::seq_iter O_I = Outputs.prepare_seq_iteration();
+    while (true) {
+      if(O_I.has_next()) {
+        node_t n = O_I.get_next();
+        printf("%d :  %f\n", G.nodeid_to_nodekey(n), BC[n]);
+      }
+      else break;
+    }
+    printf("\n");
+
+
 }
